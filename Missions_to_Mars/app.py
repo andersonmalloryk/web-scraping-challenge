359bac2676b1b41dc1c56f53fa26ea5632920526
mallory anderson <mallorykanderson@mallorys-Air.T-mobile.com>
2021-04-03 16:13:58 -0500

    attempt at starting mongo connection

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+conn = 'mongodb:///localhost:27017'
+client = pymongo.MongoClient(conn)
+
+db = client.team_db
+
+db.mars.insert_many()
+
+@app.route("/")
+def index():
+    listings = mongo.db.listings.find_one()
+    return render_template("index.html", listings=listings)
+
+
+@app.route("/scrape")
+def scraper():
+    listings = mongo.db.listings
+    listings_data = scrape_phone.scrape()
+    listings.update({}, listings_data, upsert=True)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # web-scraping-challenge
 This is the eleventh homework for the Data Analytics and Visualization Bootcamp through the University of Minnesota. This project scrapes data from the Mars News Site and saves it to a mongo database and then generate a HTML files to display key elements. 
+
+#Open Flask Server
+Open a terminal or gitbash from the folder with the files. 
+Run "python app.py" in terminal/gitbash
+Now your Flask server is running and you can copy the http://127.0.0.1:500 into a Chrome browser to continue. 
