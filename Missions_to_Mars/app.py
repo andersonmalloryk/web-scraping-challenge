22450d21abd0a4815c534e222bccebfff94751a2
mallory anderson <mallorykanderson@mallorys-Air.T-mobile.com>
2021-04-04 17:35:59 -0500

    scrape function works and connects to mongo.db

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -7,31 +7,19 @@ app = Flask(__name__)
 # Use flask_pymongo to set up mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_info")
 
+@app.route("/")
+def index():
 
-# Do I need to define the collection? 
-# db = client.nammeOfCollection
-
-# If all of the data is stored into one collection... 
-# then I can use this one route to fill in the index.html
-# # or do I need a route for each section?
-# @app.route("/")
-# def index():
-
-#     title = mongo.db.titles.find_one()
-#     paragraph = mongo.db.paragraph.find_one()
-#     featured_image_url = mongo.db.featured_image_url()
-#     hemisphere_image_urls = mongo.db.hemisphere_image_urls()
-
-#     listings = mongo.db.listings.find_one()
-#     return render_template("index.html", listings=listings)
+    features = mongo.db.features.find_one()
+    return render_template("index.html", features = features)
 
 
 @app.route("/scrape")
 def scraper():
 
-    hemisphere_image_urls = scrape_mars.scrape_info()
-    mongo.db.collection.update({}, hemisphere_image_urls, upsert=True)
-
+    features = mongo.db.features
+    scrape_content = scrape_mars.scrape_info()
+    features.update({}, scrape_content, upsert=True)
     return redirect("/", code=302)
 
 if __name__ == "__main__":
